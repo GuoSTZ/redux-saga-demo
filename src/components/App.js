f4c59845f9f9fd65cf8e08c90df4808c45c767df
guosheng <gk7018@outlook.com>
2019-10-12 11:10:39 +0800

    整体样式添加

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import DataInput from './input/index';
 import TableShow from './table/index';
 import TestForm from './modifyPerson/index';
-
+import './App.less'
 
 const App = () => (
-  <div style={{ position: 'absolute', width: '100%' }}>
+  <div className='box'>
     <DataInput />
     <TableShow />
     {/* <TestForm /> */}
