fd6dc615d474136441c0bb403adf323366bd0c5c
guosheng <gk7018@outlook.com>
2019-10-16 08:52:17 +0800

    添加redux-logger插件

--- src/store/store.js
+++ src/store/store.js
@@ -5,13 +5,14 @@ import {
 import reducerAll from '../reducers/index.js';
 import createSagaMiddleware from 'redux-saga'; // 引入redux-saga中的createSagaMiddleware函数
 import rootSaga from '../sagas/saga.js'; // 引入saga.js
+import logger from 'redux-logger'
 
 const sagaMiddleware = createSagaMiddleware() // 执行
 
 export const store = createStore(
     reducerAll,
     window.devToolsExtension ? window.devToolsExtension() : undefined, // dev-tools
-    applyMiddleware(sagaMiddleware) // 中间件，加载sagaMiddleware
+    applyMiddleware(sagaMiddleware, logger), // 中间件，加载sagaMiddleware
 )
 
 sagaMiddleware.run(rootSaga) // 执行rootSaga
