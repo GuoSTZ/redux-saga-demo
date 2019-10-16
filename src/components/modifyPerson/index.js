964b2705bd885203ec80ea629ecc7d85e1e8b108
guosheng <gk7018@outlook.com>
2019-10-16 08:55:48 +0800

    格式修改

--- src/components/modifyPerson/index.js
+++ src/components/modifyPerson/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import {
-    Modal,
-    Input,
-    Select,
-    Form
-} from 'antd';
-import {
-    connect
-} from 'react-redux';
-import {
-    modifyPerson,
-    hideModal
-} from '../../actions/index';
+import { Modal, Input, Select, Form } from 'antd';
+import { connect } from 'react-redux';
+import { modifyPerson, hideModal } from '../../actions/index';
 import './index.less';
+
 const Option = Select.Option;
-const options = [{
+const options = [
+    {
         label: '男',
         value: 1
     },
@@ -23,101 +15,68 @@ const options = [{
         value: 2
     }
 ];
-const ModifyPerson = ({
-    modal,
-    persons,
-    dispatch,
-    form
-}) => {
+const ModifyPerson = ({ modal, persons, dispatch, form }) => {
     let person = persons[modal.index]
     let name = person.name;
     let age = person.age;
     let sex = person.sex;
     let sexText = sex === 1 ? '男' : '女';
-    const {
-        getFieldDecorator
-    } = form
+    const { getFieldDecorator } = form
     let value = form.getFieldsValue();
-    return ( <
-        Modal title = "修改个人信息"
-        visible = {
-            modal.visible
-        }
-        onOk = {
-            () => {
-                dispatch(hideModal())
-                dispatch(modifyPerson(value, modal.index))
+    return (
+        <Modal title="修改个人信息"
+            visible={modal.visible}
+            onOk={
+                () => {
+                    dispatch(hideModal())
+                    dispatch(modifyPerson(value, modal.index))
+                }
             }
-        }
-        onCancel = {
-            () => dispatch(hideModal())
-        }
-        closable = {
-            false
-        }
-        okText = '确认修改'
-        cancelText = '放弃修改' >
-        <
-        Form >
-        <
-        Form.Item > {
-            getFieldDecorator('name', {
-                rules: [{
-                    required: true,
-                    message: 'Please input name!'
-                }],
-                initialValue: `${name}`
-            })( <
-                Input name = 'name'
-                placeholder = '请输入姓名'
-                className = 'modify-nameInput'
-                allowClear /
-                >
-            )
-        } <
-        /Form.Item> <
-        Form.Item > {
-            getFieldDecorator('age', {
-                rules: [{
-                    required: true,
-                    message: 'Please input age!'
-                }],
-                initialValue: `${age}`
-            })( <
-                Input placeholder = '请输入年龄'
-                className = 'modify-ageInput'
-                allowClear /
-                >
-            )
-        } <
-        /Form.Item> <
-        Form.Item > {
-            getFieldDecorator('sex', {
-                rules: [{
-                    required: true,
-                    message: 'Please input sex!'
-                }],
-                initialValue: `${sexText}`
-            })( <
-                Select className = 'modify-sexSelect' > {
-                    options.map(item => ( <
-                        Option key = {
-                            item.value
+            onCancel={() => dispatch(hideModal())}
+            closable={false}
+            okText='确认修改'
+            cancelText='放弃修改' >
+            <Form >
+                <Form.Item >
+                    {getFieldDecorator('name', {
+                        rules: [{ required: true, message: 'Please input name!' }],
+                        initialValue: `${name}`
+                    })(<Input
+                        name='name'
+                        placeholder='请输入姓名'
+                        className='modify-nameInput'
+                        allowClear />
+                    )
+                    }
+                </Form.Item>
+                <Form.Item >
+                    {getFieldDecorator('age', {
+                        rules: [{ required: true, message: 'Please input age!' }],
+                        initialValue: `${age}`
+                    })(<Input
+                        placeholder='请输入年龄'
+                        className='modify-ageInput'
+                        allowClear />
+                    )
+                    }
+                </Form.Item>
+                <Form.Item >
+                    {getFieldDecorator('sex', {
+                        rules: [{ required: true, message: 'Please input sex!' }],
+                        initialValue: `${sexText}`
+                    })(<Select
+                        className='modify-sexSelect' >
+                        {options.map(item =>
+                            (<Option key={item.value}
+                                value={item.value} > {item.label}
+                            </Option>)
+                        )
                         }
-                        value = {
-                            item.value
-                        } > {
-                            item.label
-                        } <
-                        /Option>
-                    ))
-                } <
-                /Select>
-            )
-        } <
-        /Form.Item> <
-        /Form> <
-        /Modal>
+                    </Select>)
+                    }
+                </Form.Item>
+            </Form>
+        </Modal>
     )
 }
 
@@ -129,9 +88,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        dispatch
-    }
+    return { dispatch }
 }
 const ModifyForm = Form.create()(ModifyPerson)
 // export default connect(mapStateToProps, mapDispatchToProps)(ModifyPerson);
