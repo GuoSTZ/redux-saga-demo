aca57148780ecb35ed78e29fe303bbdd997feede
guosheng <gk7018@outlook.com>
2019-10-16 08:58:40 +0800

    渲染组件

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import DataInput from './input/index';
 import TableShow from './table/index';
 import ModifyForm from './modifyPerson/index';
+import ImportButton from './saga-import/index'
 import './App.less'
 
 const App = () => (
   <div className='box'>
     <DataInput />
+    <ImportButton />
     <TableShow />
     <ModifyForm />
   </div>
