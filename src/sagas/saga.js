2443078393d96e529bd886152583299a0b0768a9
guosheng <gk7018@outlook.com>
2019-10-16 08:57:35 +0800

    格式修改

--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -1,13 +1,7 @@
-import {
-    actionTypes
-} from '../common/actionTypes.js';
-import {
-    call,
-    put,
-    takeEvery
-} from 'redux-saga/effects'; // 引入相关函数
+import { actionTypes } from '../common/actionTypes.js';
+import { call, put, takeEvery } from 'redux-saga/effects'; // 引入相关函数
 
-function* getData(action) { // 参数是action创建函数返回的action
+function* getData() { // 参数是action创建函数返回的action
     const p = function () {
         return fetch(`https://guostz.github.io/Tuan/data.json`, {
             method: 'GET'
@@ -18,17 +12,16 @@ function* getData(action) { // 参数是action创建函数返回的action
     }
     const data = yield call(p)
     if (data.code === 0) {
-        let value = data.data
+        let value = data.data.items
         yield put({ // dispatch一个action到reducer， payload是请求返回的数据
-            type: actionTypes.ADD_PERSON_SUCCESS,
+            type: actionTypes.IMPORT_PERSONS_SUCCESS,
             value
         })
     }
 }
 
 function* rootSaga() { // 在store.js中，执行了 sagaMiddleware.run(rootSaga)
-    console.log('rootSaga')
-    yield takeEvery(actionTypes.ADD_PERSON, getData) // 如果有对应type的action触发，就执行goAge()函数
+    yield takeEvery(actionTypes.IMPORT_PERSONS, getData) // 如果有对应type的action触发，就执行goAge()函数
 }
 
 export default rootSaga; // 导出rootSaga，被store.js文件import
